7f241208751f10c212e01fc15cb9bce7eb9a6111
Vishwajeet Shinde <shindevishwajeet434@gmail.com>
2024-10-11 17:27:02 +0530

    changed the allowed origin

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://glittering-lily-92e49b.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
